69288c5f91c58a370d8438ac89e33976e76aa2c6
eugene <uwm1004@gmail.com>
2021-09-19 01:09:01 +0900

    pug connect

--- README.md
+++ README.md
@@ -1,33 +1,7 @@
-nodejs+express 초기환경세팅
+router에서 사용하는 path parameter
+:id 와 같은 특수한 주소의 경우
 
-1. install npm
-```npm install init```
-2. install express
-```npm install express```
-3. install typescript
-```npm install --save-dev typescript```
-4. express를 쓰면서 type도움을 받을수있음
-```npm install --save-dev @types/express```
 
-main.js 초기 세팅
+pug로 템플릿그려서 html로 컴파일하는 방법
+[https://dubaiyu.tistory.com/211]
 
-```
-//@ts-check
-
-const express = require('express')
-const app = express()
-
-// routing
-app.use('/',(req, res)=>{
-    res.send('hello,express!')
-})
-
-app.user((req,res)=>{
-    res.send('hello2)
-})
-
-const PORT = 5000
-app.listen(PORT,()=>{
-    console.log(`The express server is listening at prot: ${PORT}`)
-})
-```
--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@
 
 const express = require('express')
 const fs = require('fs')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.json())
+
+app.use(express.static('src/public'))
 
 app.set('views', 'src/views')
 app.set('view engine', 'pug')
@@ -13,11 +17,9 @@ app.set('view engine', 'pug')
 const USERS = {
     15:{
         nickname:'foo',
-        profileImage: undefined
     },
     16:{
         nickname:'bar',
-        profileImage: undefined
 
     }
 }
@@ -28,13 +30,47 @@ userRouter.get('/', (req,res)=>{
     res.send('User list')
 })
 
+//param collback id를 넘겨받으면 어떻게 처리해라~
+userRouter.param('id',(req, res, next, value)=>{
+    console.log(`id param`,value)
+    //@ts-ignore
+    req.user = USERS[value]
+    next()
+})
+
 // /users/15
 userRouter.get('/:id', (req,res)=>{
-    res.send('User info with ID')
+    const resMineType = req.accepts(['json','html'])
+
+    if(resMineType === 'json'){
+        //@ts-ignore
+        res.send(req.user)
+    }else if(resMineType === 'html'){
+        res.render('index')
+    }else{
+        // error handling
+    }
+
+    // res.send('User info with ID')
+    // console.log('userRouter get ID')
+    //@ts-ignore
+    // res.send(req.user) //object를 express가 알아서 json을 stringify
 })
+
+
 
 userRouter.post('/',(req,res)=>{
     res.send('register user')
+})
+
+userRouter.post('/:id/nickname', (req,res)=>{
+    //@ts-ignore
+    const { user } = req
+    // req.body에서 닉네임가져오기 undefined에러 => body-purser 모듈필요
+    const { nickname } = req.body
+
+    user.nickname = nickname
+    res.send(`user nick name updated: ${nickname}`)
 })
 
 app.use('/users', userRouter)
